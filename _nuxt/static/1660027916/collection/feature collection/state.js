65afbd49c1731c73b4e5c40be39b314d5cdd90ef
Vinayak Basude <vinaybasude@yahoo.in>
2022-08-09 13:28:15 +0530

    updated files

--- _nuxt/static/1660027916/collection/feature collection/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F%3Crepository-name%3E\u002F_nuxt\u002Fstatic\u002F1660027916",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcollection\u002Ffeature%20collection",config:{_app:{basePath:"\u002F%3Crepository-name%3E\u002F",assetsPath:"\u002F%3Crepository-name%3E\u002F_nuxt\u002F",cdnURL:a}}}}(null));
